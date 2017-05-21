3a5b33797e0b930b09615eb49c8cc5791f87a41f
jotaiwan <jotaiwan@gmail.com>
2017-05-21 22:24:56 +1000

    chapter01: try

--- src/main/java/com/practices/HelloWorld.java
+++ src/main/java/com/practices/HelloWorld.java
@@ -1,13 +1,21 @@
 package com.practices;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by jotaiwan on 20/05/2017.
  */
 public class HelloWorld {
 
+    GoodWorld goodWorld;
+
     private String helloMessage;
 
     private String goodMessage;
+
+    public HelloWorld() {
+        setGoodMessage(goodWorld.getGoodMessage());
+    }
 
     public String getHelloMessage() {
         return helloMessage;
