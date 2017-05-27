df1ff46f769140042403025a5a08c82f37c39ff6
jotaiwan <jotaiwan@gmail.com>
2017-05-28 00:22:46 +1000

    update codes

---
+++ src/main/java/com/chapter02/autoconfig/SgtPeppersNamed.java
@@ -0,0 +1,18 @@
+package com.chapter02.autoconfig;
+
+import org.springframework.stereotype.Component;
+import javax.inject.Named;
+
+
+/**
+ * Created by jotaiwan on 27/05/2017.
+ */
+@Named("SgtPeppersNamed")
+public class SgtPeppersNamed {
+    private String title = "Sgt. Pepper's Lonely Hearts Club Band - Named";
+    private String artistt = "The Beatles - Named";
+
+    public void play2() {
+        System.out.println("Playing " + title + " by " + artistt);
+    }
+}
